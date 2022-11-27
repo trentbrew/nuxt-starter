a2f46de1eb17879fbb9a0a196982da3795fc4351
Trent Brew <tbrew1023@gmail.com>
2022-11-27 00:55:57 -0800

    genesis

---
+++ nuxt.config.js
@@ -0,0 +1,15 @@
+export default defineNuxtConfig({
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+const state = reactive({
+  firstName: 'Trent',
+  lastName: 'Brew',
+})
+</script>
+
+<template>
+  <div class="bg-red">
+    <h1 class="text-xl">Welcome</h1>
+    <p>{{ `${state.firstName} ${state.lastName}` }}</p>
+  </div>
+</template>
