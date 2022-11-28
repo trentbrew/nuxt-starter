7558ebc8586deb62bdeec73bfc8b4b27ccd0e4d2
Trent Brew <tbrew1023@gmail.com>
2022-11-28 05:31:53 -0800

    layouts and header component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <nav class="absolute flex justify-center items-center gap-4 w-full border-[#ffffff22] border-b-2 list-none h-[80px]">
-      <nuxt-link class="hover:opacity-50" to="/"><li>Home</li></nuxt-link>
-      <nuxt-link class="hover:opacity-50" to="/about"><li>About</li></nuxt-link>
-    </nav>
+    <Navigation />
     <div class="w-full h-screen flex flex-col justify-center items-center">
       <NuxtPage />
     </div>
