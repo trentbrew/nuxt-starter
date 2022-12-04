162a5c290a85296dffb050242307bfd42cf016e0
Trent Brew <tbrew1023@gmail.com>
2022-12-04 06:24:00 -0600

    scroll to top on reload

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
+<script setup>
+  const nuxtApp = useNuxtApp()
+
+  nuxtApp.hook('page:finish', () => {
+    window.scrollTo(0, 0)
+  })
+</script>
+
 <template>
   <Html data-theme="business">
     <Head><Title>Nuxt 3 Template</Title></Head>
