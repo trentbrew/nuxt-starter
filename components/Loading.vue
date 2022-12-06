db0946456cc10032f36ca92ed01e1f80d9f75af0
Trent Brew <tbrew1023@gmail.com>
2022-12-06 03:27:13 -0600

    loading indicator component

---
+++ components/Loading.vue
@@ -0,0 +1,14 @@
+<template>
+  <Icon name="loading" class="loading" />
+</template>
+
+<style scoped>
+  .loading {
+    animation: loading 1s steps(8, end) infinite;
+  }
+  @keyframes loading {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+</style>
